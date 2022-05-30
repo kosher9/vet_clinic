28ed5be4c8ca8a8d27ebd2075a854c3d28913b18
kosher <lionel.kossi@gmail.com>
2022-05-30 11:41:25 +0000

    Insert some values

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name varchar(100),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(40),
     date_of_birth DATE,
-    escape_attempts INTEGER NOT NULL DEFAULT 0,
-    neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL(5,2) NOT NULL DEFAULT 0.00
+    escape_attempts INT,
+    neutered BOOLEAN ,
+    weight_kg DECIMAL
 );

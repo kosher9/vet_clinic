90579b6c5fb89a65cf74a507ea053c0f93670d36
kosher <lionel.kossi@gmail.com>
2022-05-31 00:57:32 +0000

    Add queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,10 @@ SELECT (name, escape_attempts) FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true ;
 SELECT * FROM animals WHERE name IS DISTINCT FROM 'Gabumon' ;
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3 ;
+
+SELECT COUNT (*) as number_of_animals FROM animals;
+SELECT COUNT (*) as number_no_escape FROM animals WHERE escape_attempts = 0;
+SELECT AVG (weight_kg) as average_weight FROM animals;
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-01-01' GROUP BY species;

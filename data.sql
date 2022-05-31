523d046daaa5a2a665d7d4a2548410db78ba36a7
kosher <lionel.kossi@gmail.com>
2022-05-31 01:01:13 +0000

    update animals

--- data.sql
+++ data.sql
@@ -4,3 +4,4 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', 'Nov 15, 2018', 8, true, 2);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', 'Jan 7, 2021', 15.04, false, 1);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', 'May 12, 2017', 11, true, 5);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Charmander', 'Feb 8, 2020', 11, false, 0), ('Plantmon', 'Nov 15, 2021', 5.7, true, 2), ('Squirtle', 'Apr 2, 1993', 12.13, true, 3), ('Angemon', 'Jun 12, 2005', 45, true, 1), ('Boarmon', 'Jun 7, 2005', 20.4, true, 7), ('Blossom', 'Oct 13, 1998', 17, true, 3), ('Ditto', 'May 14, 2022', 22, true, 4);

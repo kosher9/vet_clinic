678bb1b1ce0000202f91468bfeb782f41a7a571d
kosher <lionel.kossi@gmail.com>
2022-05-31 15:49:30 +0000

    Insert owners

--- data.sql
+++ data.sql
@@ -5,3 +5,5 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', 'Jan 7, 2021', 15.04, false, 1);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', 'May 12, 2017', 11, true, 5);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Charmander', 'Feb 8, 2020', 11, false, 0), ('Plantmon', 'Nov 15, 2021', 5.7, true, 2), ('Squirtle', 'Apr 2, 1993', 12.13, true, 3), ('Angemon', 'Jun 12, 2005', 45, true, 1), ('Boarmon', 'Jun 7, 2005', 20.4, true, 7), ('Blossom', 'Oct 13, 1998', 17, true, 3), ('Ditto', 'May 14, 2022', 22, true, 4);
+
+INSERT INTO owners (full_name, age) VALUES('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);

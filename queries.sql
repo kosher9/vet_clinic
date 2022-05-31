524f5520d08e0db988d35cd99e9ada167aec856a
kosher <lionel.kossi@gmail.com>
2022-05-31 16:11:43 +0000

    Remove species column

--- queries.sql
+++ queries.sql
@@ -59,3 +59,5 @@ SELECT AVG (weight_kg) as average_weight FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-01-01' GROUP BY species;
+
+ALTER TABLE animals DROP COLUMN species;
